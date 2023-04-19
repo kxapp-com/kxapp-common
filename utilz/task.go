eefde2e91e19c6a4adc7c583522823abd762bf00
li_li_li87222 <li_li_li87222@163.com>
2023-04-19 15:42:34 +0800

    原来的多任务是有问题的，改进

--- utilz/task.go
+++ utilz/task.go
@@ -53,56 +53,56 @@ func MultiThreadTask[T any](slice []T, numThreads int, taskFunc func(data T)) {
 workCount 设置0表示使用len(datas)个工人，设置-1表示有多少个cpu内核就多少个工人，如果工人数超过datas的size，则工人数会设置为datas的size.
 返回的错误数组，如果在执行任务过程中产生了奔溃错误，例如空指针错误，且在任务函数内没处理，则会在taskwork中recovery，避免整个程序奔溃，并且错误会放在返回结果中
 */
-func TaskWork[T any](datas []T, task func(workId int, data T), workCount int) []*TaskWorkCrash[T] {
-	var errList []*TaskWorkCrash[T]
-	size := len(datas)
-	if size == 0 {
-		return errList
-	}
-	if workCount < 0 {
-		workCount = runtime.NumCPU()
-	}
-	if workCount == 0 || workCount > size {
-		workCount = size
-	}
-	var waiter sync.WaitGroup
-	waiter.Add(size)
-	queue := make(chan T)
-
-	writer := func() {
-		for i := 0; i < size; i++ {
-			queue <- datas[i]
-		}
-		close(queue)
-	}
-	reader := func(id int) {
-		for {
-			v, ok := <-queue
-			if ok {
-				func() {
-					defer func() {
-						if err := recover(); err != nil {
-							switch err.(type) {
-							case nil:
-							default:
-								crash := &TaskWorkCrash[T]{data: v, error: err}
-								errList = append(errList, crash)
-							}
-						}
-					}()
-					task(id, v)
-				}()
-				waiter.Done()
-			} else {
-				break
-			}
-		}
-	}
-	go writer()
-
-	for i := 0; i < workCount; i++ {
-		go reader(i)
-	}
-	waiter.Wait()
-	return errList
-}
+//func TaskWork[T any](datas []T, task func(workId int, data T), workCount int) []*TaskWorkCrash[T] {
+//	var errList []*TaskWorkCrash[T]
+//	size := len(datas)
+//	if size == 0 {
+//		return errList
+//	}
+//	if workCount < 0 {
+//		workCount = runtime.NumCPU()
+//	}
+//	if workCount == 0 || workCount > size {
+//		workCount = size
+//	}
+//	var waiter sync.WaitGroup
+//	waiter.Add(size)
+//	queue := make(chan T)
+//
+//	writer := func() {
+//		for i := 0; i < size; i++ {
+//			queue <- datas[i]
+//		}
+//		close(queue)
+//	}
+//	reader := func(id int) {
+//		for {
+//			v, ok := <-queue
+//			if ok {
+//				func() {
+//					defer func() {
+//						if err := recover(); err != nil {
+//							switch err.(type) {
+//							case nil:
+//							default:
+//								crash := &TaskWorkCrash[T]{data: v, error: err}
+//								errList = append(errList, crash)
+//							}
+//						}
+//					}()
+//					task(id, v)
+//				}()
+//				waiter.Done()
+//			} else {
+//				break
+//			}
+//		}
+//	}
+//	go writer()
+//
+//	for i := 0; i < workCount; i++ {
+//		go reader(i)
+//	}
+//	waiter.Wait()
+//	return errList
+//}
